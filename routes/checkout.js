89901510098e1cea691013322243c12bccbfa3af
K. D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2019-12-01 02:50:50 +0530

    Add checkout page

---
+++ routes/checkout.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    res.render('checkout', {
+        subtotal: '13800.00',
+        estdelivery: '800.00',
+        withdelivery: '14600.00',
+        items: [
+            {
+                id: '5de2acbf14d983e4e097b174',
+                product: 'SUPPORTAL NEW XUMONK ZENSOR FOR SPORTS PERSON',
+                variant: 'Blue Colored Variant',
+                image: '/img/product/p2.jpg',
+                unitprice: '4900.00',
+                quantity: 1,
+                totalprice: '4900.00',
+            },
+            {
+                id: '5de2acbf2a9751c69c33133d',
+                product: 'SUPPORTAL NEW XUMONK ZENSOR FOR SPORTS PERSON',
+                variant: 'Red Colored Variant',
+                image: '/img/product/p7.jpg',
+                unitprice: '8900.00',
+                quantity: 1,
+                totalprice: '8900.00',
+            },
+        ],
+    });
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,6 @@ router.use('/', require('./root'));
 router.use('/category/', require('./category'));
 router.use('/item/', require('./item'));
 router.use('/cart/', require('./cart'));
+router.use('/checkout/', require('./checkout'));
 
 module.exports = router;
