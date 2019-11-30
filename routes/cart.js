6c7010f28ef4128061a203362ee0f670a36ce23f
K. D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2019-12-01 01:16:20 +0530

    Added working cart

--- routes/cart.js
+++ routes/cart.js
@@ -1,7 +1,29 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.render('cart');
+    res.render('cart', {
+        subtotal: 'LKR13800.00',
+        items: [
+            {
+                id: '5de2acbf14d983e4e097b174',
+                product: 'SUPPORTAL NEW XUMONK ZENSOR FOR SPORTS PERSON',
+                variant: 'Blue Colored Variant',
+                image: '/img/product/p2.jpg',
+                unitprice: 'LKR4900.00',
+                quantity: 1,
+                totalprice: 'LKR4900.00',
+            },
+            {
+                id: '5de2acbf2a9751c69c33133d',
+                product: 'SUPPORTAL NEW XUMONK ZENSOR FOR SPORTS PERSON',
+                variant: 'Red Colored Variant',
+                image: '/img/product/p7.jpg',
+                unitprice: 'LKR8900.00',
+                quantity: 1,
+                totalprice: 'LKR8900.00',
+            },
+        ],
+    });
 });
 
 module.exports = router;
