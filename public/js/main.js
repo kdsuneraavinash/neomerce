891c1891745b98884477b8dc45717ba518eb1a73
K. D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2019-11-30 21:26:26 +0530

    Added completed category page

--- public/js/main.js
+++ public/js/main.js
@@ -243,48 +243,6 @@ $(document).ready(function () {
     })
 
 
-
-    //----- Active No ui slider --------//
-
-
-
-    $(function () {
-
-        if (document.getElementById("price-range")) {
-
-            var nonLinearSlider = document.getElementById('price-range');
-
-            noUiSlider.create(nonLinearSlider, {
-                connect: true,
-                behaviour: 'tap',
-                start: [500, 4000],
-                range: {
-                    // Starting at 500, step the value by 500,
-                    // until 4000 is reached. From there, step by 1000.
-                    'min': [0],
-                    '10%': [500, 500],
-                    '50%': [4000, 1000],
-                    'max': [10000]
-                }
-            });
-
-
-            var nodes = [
-                document.getElementById('lower-value'), // 0
-                document.getElementById('upper-value')  // 1
-            ];
-
-            // Display the slider value and how far the handle moved
-            // from the left edge of the slider.
-            nonLinearSlider.noUiSlider.on('update', function (values, handle, unencoded, isTap, positions) {
-                nodes[handle].innerHTML = values[handle];
-            });
-
-        }
-
-    });
-
-
     //-------- Have Cupon Button Text Toggle Change -------//
 
     $('.have-btn').on('click', function (e) {
