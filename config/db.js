364e9514c66af65e537196ac55abbe000c0425a1
Lahiru <lahirudesilva.17@cse.mrt.ac.lk>
2019-12-18 19:56:58 +0530

    after pull request

--- config/db.js
+++ config/db.js
@@ -5,7 +5,9 @@ const pool = new Pool({
     host: process.env.DATABASE_HOST,
     database: process.env.DATABASE_NAME,
     password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
+    port: process.env.DATABASE_PORT
 });
 
 module.exports = pool;
+
+   
