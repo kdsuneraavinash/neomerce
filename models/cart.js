441b778047fc58e345ca6871e3960b83b5342c4d
Anju Chamantha <chamantha97anju@gmail.com>
2019-12-23 09:04:17 +0530

    showing cart in UI completed with dumy data

--- models/cart.js
+++ models/cart.js
@@ -12,17 +12,33 @@ const getCartItems = async (sessionID) => {
     let get_cart_items_query = `select * from cartitem where customer_id = $1`;
     const out_cart_items = await connection.query(get_cart_items_query, [customerID]);
     const cartItems = out_cart_items.rows;
+    const itemCount = out_cart_items.rowCount;
     console.log("Cart Items :");
     console.log(cartItems);
-    // id: '5de2acbf14d983e4e097b174',
-    // product: 'SUPPORTAL NEW XUMONK ZENSOR FOR SPORTS PERSON',
-    // variant: 'Blue Colored Variant',
-    // image: '/img/product/p2.jpg',
-    // unitprice: 'LKR4900.00',
-    // quantity: 1,
-    // totalprice: 'LKR4900.00',
-
-    return null;
+    // id: '5de2acbf14d983e4e097b174',                                  variant_id(variant tbl)
+    // product: 'SUPPORTAL NEW XUMONK ZENSOR FOR SPORTS PERSON',        product_id(variant tbl) -> 
+    // variant: 'Blue Colored Variant',                                 title(variant tbl) , attribute_name(variantattribute tbl) : attribute_value(variant tbl)
+    // image: '/img/product/p2.jpg',                                    image_url(productimage tbl)
+    // unitprice: 'LKR4900.00',                                         selling_price(variant tbl)
+    // quantity: 1,                                                     #
+    // totalprice: 'LKR4900.00',                                        = unitprice*quantity
+   
+   console.log("Looping the cartItem list : ");
+    var items = [];
+    for (i = 0; i < itemCount; i++) {
+        var item = {
+            id: cartItems[i].variant_id,
+            product: "Test Product",
+            variant : "Red Color Variant",
+            image: "http://ecx.images-amazon.com/images/I/51W35ZG1PWL._SY300_.jpg",
+            unitprice : "LKR 2000",
+            quantity: cartItems[i].quantity,
+            totalprice : "LKR 2000"
+        };
+        items.push(item);
+    };
+    // console.log(items);
+    return items;
 };
 
 
--- routes/cart.js
+++ routes/cart.js
@@ -2,35 +2,37 @@ const router = require('express').Router();
 const helper = require('../utils/helper');
 const Cart = require('./../models/cart');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     try {
         const loggedIn = req.session.user != null;
-        console.log(req.sessionID);
-        cartItems = Cart.getCartItems(req.sessionID);
+
+        const cartItems = await Cart.getCartItems(req.sessionID);
+        console.log(cartItems);
 
         res.render('cart', {
             loggedIn,
             subtotal: 'LKR13800.00',
-            items: [
-                {
-                    id: '5de2acbf14d983e4e097b174',
-                    product: 'SUPPORTAL NEW XUMONK ZENSOR FOR SPORTS PERSON',
-                    variant: 'Blue Colored Variant',
-                    image: '/img/product/p2.jpg',
-                    unitprice: 'LKR4900.00',
-                    quantity: 1,
-                    totalprice: 'LKR4900.00',
-                },
-                {
-                    id: '5de2acbf2a9751c69c33133d',
-                    product: 'SUPPORTAL NEW XUMONK ZENSOR FOR SPORTS PERSON',
-                    variant: 'Red Colored Variant',
-                    image: '/img/product/p7.jpg',
-                    unitprice: 'LKR8900.00',
-                    quantity: 1,
-                    totalprice: 'LKR8900.00',
-                },
-            ],
+            items: cartItems
+            // items: [
+            //     {
+            //         id: '5de2acbf14d983e4e097b174',
+            //         product: 'SUPPORTAL NEW XUMONK ZENSOR FOR SPORTS PERSON',
+            //         variant: 'Blue Colored Variant',
+            //         image: '/img/product/p2.jpg',
+            //         unitprice: 'LKR4900.00',
+            //         quantity: 1,
+            //         totalprice: 'LKR4900.00',
+            //     },
+            //     {
+            //         id: '5de2acbf2a9751c69c33133d',
+            //         product: 'SUPPORTAL NEW XUMONK ZENSOR FOR SPORTS PERSON',
+            //         variant: 'Red Colored Variant',
+            //         image: '/img/product/p7.jpg',
+            //         unitprice: 'LKR8900.00',
+            //         quantity: 1,
+            //         totalprice: 'LKR8900.00',
+            //     },
+            // ],
         });
     } catch (error) {
         helper.errorResponse(res, error);
