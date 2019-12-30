5dcc623b94590cadd97425e25c653e3f8624aff0
K. D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2019-12-31 00:24:18 +0530

    Admin can now see order reports

--- database/schema.sql
+++ database/schema.sql
@@ -167,9 +167,13 @@ CREATE OR REPLACE FUNCTION checkOrderHistoryPriviledge(SESSION_UUID,UUID4) RETUR
 $$
 DECLARE
 customer_id1 uuid4 := (select customer_id from session where session_id=$1);
+account_type varchar(15) := (select account_type from session join customer using(customer_id) where session_id=$1);
 customer_id2 uuid4 := (select customer_id from orderdata where order_id = $2); 
 BEGIN
-	if customer_id1 = customer_id2 then 
+    if account_type = 'admin' then
+        return true;
+	end if; 
+    if customer_id1 = customer_id2 then 
         return true;
 	else
 	    return false;
