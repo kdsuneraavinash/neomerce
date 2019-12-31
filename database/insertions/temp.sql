3cba41167c0cd147aa5fa92b9139a603deddbe3c
Kasun Tharaka <mornk1@gmail.com>
2019-12-31 14:40:32 +0530

    Removed unwanted files

--- database/insertions/temp.sql
+++
@@ -1,9 +0,0 @@
-select product_id, product.title, sum(orderitem.quantity) as quantity, 
-        sum(orderitem.quantity*variant.selling_price) as income from 
-        (select order_id from orderdata
-        where extract(quarter from orderdata.order_date)=4 and 
-        extract(year from orderdata.order_date)=2019) as req_orders
-            join orderitem using(order_id)
-            join variant using(variant_id)
-            join product using(product_id)
-        group by product_id, product.title
