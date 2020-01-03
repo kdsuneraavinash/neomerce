866748218892e96695af90f39372ae2e20a22c0b
K. D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2020-01-03 15:52:52 +0530

    Added procfile

--- config/db.js
+++ config/db.js
@@ -1,13 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: process.env.DATABASE_USER,
-    host: process.env.DATABASE_HOST,
-    database: process.env.DATABASE_NAME,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT
+    connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = pool;
-
-   
