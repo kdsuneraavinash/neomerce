a2baf7d6753256e911b208c0800eb3fc44b6ebfa
K. D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2020-01-04 18:49:19 +0530

    Chanegd cookie period

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(session({
     saveUninitialized: false,
     secret: process.env.SESSION_SECRET,
     resave: false,
-    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 },
+    cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 },
 }));
 
 
